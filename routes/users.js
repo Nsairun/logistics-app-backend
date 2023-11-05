b9fc947f92fd6b3c47c55c0802741dee9dc26c72
unknown <emmansairun@gmail.com>
2023-11-05 18:51:16 +0100

    firdt commit

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+// const express = require('express');
+// const router = express.Router();
+// const { signup, signin, signout } = require('../controllers/user');
+// const { check } = require('express-validator');
+
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// router.post('/signup', [
+//   check('name').isLength({ min: 3 }).withMessage("Name should be at least 3 characters"),
+//   check('email').isEmail().withMessage("Email should be valid"),
+//   check('password').isLength({ min: 6 }).withMessage("Password should be at least 6 characters")
+// ], signup);
+
+// router.post('/signin', signin);
+
+// router.get('/signout', signout);
+
+// module.exports = router;
